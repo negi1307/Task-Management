c2e9c467da8c0e217b9a1a469fd983d12052bf41
Ritika <ritika82788@gmail.com>
2023-11-09 09:53:20 +0530

    new

--- admin/src/pages/Task-Manager/board/taskDetailPage.js
+++ admin/src/pages/Task-Manager/board/taskDetailPage.js
@@ -142,8 +142,8 @@ const TaskDetailPage = ({ modal, editData, closeModal }) => {
                                                             <h4 className="m-0 p-0"> {ele?.userId?.firstName}</h4>
                                                             <h4 className="ps-1 m-0 p-0"> {ele?.userId?.lastName}</h4>
                                                             <p className="ps-1 m-0 p-0">
-                                                                {/* {moment(ele?.createdAt).format('LT')}{' '} */}
-                                                                {moment(ele?.createdAt).calendar()  }
+                                                                {moment(ele?.createdAt).format('LT')}{' '}
+                                                                {/* {moment(ele?.createdAt).add(1, 'days').calendar()}     */}
                                                             </p>
                                                             {/* <p className='ps-1 m-0 p-0'>{moment(ele?.createdAt).startOf('hour').fromNow()}</p> */}
                                                         </div>
