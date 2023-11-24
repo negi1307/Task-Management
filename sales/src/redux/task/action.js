0fd98f04ff9f8f442d6e3bde5320d9ee6b997023
Ritika <ritika82788@gmail.com>
2023-11-24 18:32:24 +0530

    sales pannel

---
+++ sales/src/redux/task/action.js
@@ -0,0 +1,57 @@
+import TASK_TYPES from "./constant";
+
+type AuthAction = { type: string, payload: {} | string };
+
+export const createTask = (data): AuthAction => ({
+    type: TASK_TYPES.CREATE_TASK,
+    payload: data
+})
+export const getsingleSprintTask = (data): AuthAction => ({
+    type: TASK_TYPES.GET_SINGLE_SPRINT_TASK,
+    payload: data
+})
+export const getAllTask= (data): AuthAction => ({
+    type: TASK_TYPES.GET_ALL_TASK,
+    payload: data
+})
+export const updateTask= (data): AuthAction => ({
+    type: TASK_TYPES.UPDATE_TASK,
+    payload: data
+})
+export const deleteTask = (data): AuthAction => ({
+    type: TASK_TYPES.DELETE_TASK,
+    payload: data
+})
+
+export const updateTaskStatus = (data): AuthAction => ({
+    type: TASK_TYPES.UPDATE_TASK_STATUS,
+    payload: data
+})
+export const TaskStatusAction = (data): AuthAction => ({
+    type: TASK_TYPES.TASK_STATUS,
+    payload: data
+})
+export const AddComment= (data): AuthAction => ({
+    type: TASK_TYPES.ADD_COMMENT,
+    payload: data
+})
+export const getComment= (data): AuthAction => ({
+    type: TASK_TYPES.GET_COMMENT,
+    payload: data
+})
+export const deleteComment= (data): AuthAction => ({
+    type: TASK_TYPES.DELETE_COMMENT,
+    payload: data
+})
+export const UpdateCommentAction= (data): AuthAction => ({
+    type: TASK_TYPES.UPDATE_COMMENT,
+    payload: data
+})
+export const gettaskId= (data): AuthAction => ({
+    type: "taskid",
+    payload: data
+})
+export const getAssignUserAction= (data): AuthAction => ({
+    type: TASK_TYPES.GET_ASSIGN_USER,
+    payload: data
+})
