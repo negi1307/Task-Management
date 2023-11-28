c22aa2b2afe3e1747fbd5e219cb9c22944f31122
Ritika <ritika82788@gmail.com>
2023-11-28 12:11:44 +0530

    uppdate coustmer api

--- admin/src/pages/Task-Manager/customer/CustomerIndex.js
+++ admin/src/pages/Task-Manager/customer/CustomerIndex.js
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import HeaderMain from '../header/HeaderMain';
-import { Button, Card ,Table} from 'react-bootstrap';
-import {deletePreSalesData, getPreSalesData } from '../../../redux/customer/action';
+import { Button, Card, Table, Row, Col } from 'react-bootstrap';
+import { deletePreSalesData, getPreSalesData } from '../../../redux/customer/action';
 import Create from './modal/create';
-import MainDeleteModel from "../../../constants/deleteModel/MainDeleteModel"
-import Edit from "./modal/edit"
-import { useSelector, useDispatch } from "react-redux";
-import ToastHandle from "../../../constants/toaster/toaster";
+import MainDeleteModel from '../../../constants/deleteModel/MainDeleteModel';
+import Edit from './modal/edit';
+import { useSelector, useDispatch } from 'react-redux';
+import ToastHandle from '../../../constants/toaster/toaster';
 import MainLoader from '../../../constants/Loader/loader';
 import { getAllTechnology } from '../../../redux/technology/action';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 const CustomerIndex = () => {
     const store = useSelector((state) => state);
-   
-    const dispatch = useDispatch()
+
+    const dispatch = useDispatch();
 
     const [createModalOpen, setCreateModalOpen] = useState(false);
     const [render, setRender] = useState(false);
+    const [skip, setSkip] = useState(1);
     const createhandle = () => {
         setCreateModalOpen(true);
     };
@@ -23,43 +26,46 @@ const CustomerIndex = () => {
         if (val == 'render') {
             setRender(!render);
         }
-       
+
         setCreateModalOpen(false);
-        setEditModalOpen(false)
+        setEditModalOpen(false);
+    };
+    const handlePaginationChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        setSkip(value);
+        dispatch(getPreSalesData({ skip: value }));
     };
     useEffect(() => {
-        dispatch(getPreSalesData());
+        dispatch(getPreSalesData({ skip: 1 }));
     }, [render]);
-    const preSaleData = store?.getPreSaleReducer?.data?.response
-    const preSaleLoading = store?.getPreSaleReducer?.loading
+    const preSaleData = store?.getPreSaleReducer?.data?.response;
+    const preSaleLoading = store?.getPreSaleReducer?.loading;
     const preSaleDeleteStatus = store?.deletePreSaleReducer?.deletePreSale?.status;
     const preSaleDeleteMessage = store?.deletePreSaleReducer?.deletePreSale?.message;
 
-
     // delete model
-    const [deleteModel,setDeleteModel]=useState(false)
-    const [deleteId,setDeleteId]=useState('')
-    const [EditModalOpen,setEditModalOpen]=useState(false)
-    const [editData,setEditData] =useState()
-    const comfimDelete=()=>{
-        dispatch(deletePreSalesData(deleteId)) 
-    }
-    const deleteIdGet=(id)=>{
-        setDeleteId(id)
-        setDeleteModel(true)
-    }
-const EditModalhandle=(ele)=>{
-    setEditModalOpen(true)
-    setEditData(ele)
-}
+    const [deleteModel, setDeleteModel] = useState(false);
+    const [deleteId, setDeleteId] = useState('');
+    const [EditModalOpen, setEditModalOpen] = useState(false);
+    const [editData, setEditData] = useState();
+    const comfimDelete = () => {
+        dispatch(deletePreSalesData(deleteId));
+    };
+    const deleteIdGet = (id) => {
+        setDeleteId(id);
+        setDeleteModel(true);
+    };
+    const EditModalhandle = (ele) => {
+        setEditModalOpen(true);
+        setEditData(ele);
+    };
     useEffect(() => {
-        if (preSaleDeleteStatus === "200") {
+        if (preSaleDeleteStatus === '200') {
             ToastHandle('success', preSaleDeleteMessage);
-            dispatch(getPreSalesData());
-            setDeleteModel(false)
+            dispatch(getPreSalesData({ skip: 1 }));
+            setDeleteModel(false);
         }
-        dispatch(getAllTechnology())
-    }, [preSaleDeleteStatus])
+        dispatch(getAllTechnology());
+    }, [preSaleDeleteStatus]);
     return (
         <>
             <Card>
@@ -96,78 +102,124 @@ const EditModalhandle=(ele)=>{
                         </div>
                     </div>
                     <div>
-                    <div>
-            {preSaleLoading ? (
-                <MainLoader />
-            ) : (
-                <Table className="mb-0 add_Color_font" striped>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th> First Name</th>
-                            <th>Project</th>
-                            <th>Description</th>
-                            <th> Status</th>
-                            <th> Type</th>
-                            <th> Stage</th>
-                            <th> Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <>
-                            {preSaleData?.map((ele, ind) => {
-                                console.log(ele?.stage, '=-')
-                                return (
-                                    <tr className="align-middle">
-                                        <th scope="row">{ind + 1}</th>
-                                        <td className="cp">
-                                            <span className="namelink"> {ele?.clientName} </span>
-                                        </td>
-                                        <td className="cp">
-                                            <span className="namelink"> {ele?.projectName} </span>
-                                        </td>
-                                        <td className="w-20">
-                                            <span className="namelink"> {ele?.description?.slice(0, 10)
-                                                .concat("...")}</span>
-                                        </td>
-                                        <td>
-                                            <span className="namelink">
-                                                {ele?.status === 1 ?
-                                                    <>CONVERTED</> :
-                                                    ele?.status === 2 ? <>NOT-CONVERTED</> : ""}
-                                            </span>
-                                        </td>
-                                        <td>
-                                            <span className="namelink">
-                                                {ele?.stage === 1 ? <>HOT</> : ele?.stage === 2 ? <>COLD</> : ele?.stage === 3 ? <>MEDIUM</> : <></>}
-                                            </span>
-                                        </td>
-                                        <td>
-                                            <span className="namelink">
-                                             
-                                            </span>
-                                        </td>
-                                        <td>
-                                            <span className="namelink">
-                                                <span>
-                                                    <i class="bi bi-pencil-square" onClick={() => { EditModalhandle( ele) }}></i>
-                                                </span>
-                                                <span className="ms-2">
-                                                    <i class="bi bi-trash" onClick={() =>  {deleteIdGet(ele?._id)}}></i>
-                                                </span>
-                                            </span>
-                                        </td>
-                                    </tr>
-                                );
-                            })}
-                        </>
-                    </tbody>
-                </Table>
-            )}
-            <MainDeleteModel deleteModel={deleteModel} onHide={()=>setDeleteModel(false)} comfimDelete={comfimDelete}/>
-            <Edit modal={EditModalOpen} editData={editData} closemodal={closeModal}/>
-        </div>
-                </div>
+                        <div>
+                            {preSaleLoading ? (
+                                <MainLoader />
+                            ) : (
+                                <Table className="mb-0 add_Color_font" striped>
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th> First Name</th>
+                                            <th>Project</th>
+                                            <th>Description</th>
+                                            <th> Status</th>
+                                            <th> Type</th>
+                                            <th> Stage</th>
+                                            <th> Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <>
+                                            {preSaleData?.map((ele, ind) => {
+                                                console.log(ele?.stage, '=-');
+                                                return (
+                                                    <tr className="align-middle">
+                                                        <th scope="row">{(skip - 1) * 10 + ind + 1}</th>
+                                                        <td className="cp">
+                                                            <span className="namelink"> {ele?.clientName} </span>
+                                                        </td>
+                                                        <td className="cp">
+                                                            <span className="namelink"> {ele?.projectName} </span>
+                                                        </td>
+                                                        <td className="w-20">
+                                                            <span className="namelink">
+                                                                {' '}
+                                                                {ele?.description?.slice(0, 10).concat('...')}
+                                                            </span>
+                                                        </td>
+                                                        <td>
+                                                            <span className="namelink">
+                                                                {ele?.status === 1 ? (
+                                                                    <>CONVERTED</>
+                                                                ) : ele?.status === 2 ? (
+                                                                    <>NOT-CONVERTED</>
+                                                                ) : (
+                                                                    ''
+                                                                )}
+                                                            </span>
+                                                        </td>
+                                                        <td>
+                                                            <span className="namelink">
+                                                                {ele?.stage === 1 ? (
+                                                                    <>HOT</>
+                                                                ) : ele?.stage === 2 ? (
+                                                                    <>COLD</>
+                                                                ) : ele?.stage === 3 ? (
+                                                                    <>MEDIUM</>
+                                                                ) : (
+                                                                    <></>
+                                                                )}
+                                                            </span>
+                                                        </td>
+                                                        <td>
+                                                            <span className="namelink">
+                                                                {' '}
+                                                                {ele?.type?.map((ele) => (
+                                                                    <p className="p-0 m-0">{ele}</p>
+                                                                ))}
+                                                            </span>
+                                                        </td>
+                                                        <td>
+                                                            <span className="namelink">
+                                                                <span>
+                                                                    <i
+                                                                        class="bi bi-pencil-square"
+                                                                        onClick={() => {
+                                                                            EditModalhandle(ele);
+                                                                        }}></i>
+                                                                </span>
+                                                                <span className="ms-2">
+                                                                    <i
+                                                                        class="bi bi-trash"
+                                                                        onClick={() => {
+                                                                            deleteIdGet(ele?._id);
+                                                                        }}></i>
+                                                                </span>
+                                                            </span>
+                                                        </td>
+                                                    </tr>
+                                                );
+                                            })}
+                                        </>
+                                    </tbody>
+                                </Table>
+                            )}
+                            <Row>
+                                <Col
+                                    lg={12}
+                                    className="d-flex justify-content-end my-3 pe-4 position-absolute bottom-0">
+                                    {store?.getPreSaleReducer?.data?.totalPages > 0 && (
+                                        <Stack spacing={2}>
+                                            <Pagination
+                                                defaultPage={skip}
+                                                count={store?.getPreSaleReducer?.data?.totalPages}
+                                                color="primary"
+                                                variant="outlined"
+                                                onChange={handlePaginationChange}
+                                            />
+                                        </Stack>
+                                    )}
+                                </Col>
+                            </Row>
+                            <MainDeleteModel
+                                deleteModel={deleteModel}
+                                onHide={() => setDeleteModel(false)}
+                                comfimDelete={comfimDelete}
+                            />
+                            <Edit modal={EditModalOpen} editData={editData} closemodal={closeModal} />
+                        </div>
+                    </div>
                     <div>
                         <>
                             <Create modal={createModalOpen} closemodal={closeModal} />
