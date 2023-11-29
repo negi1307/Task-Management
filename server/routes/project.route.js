b7fc70b27b0f55ecb78ca6e50145eb4d7a6c1753
FIRST_NAME LAST_NAME <89856336+LeenaDhiman@users.noreply.github.com>
2023-11-29 11:19:14 +0530

    remove the project download system

--- server/routes/project.route.js
+++ server/routes/project.route.js
@@ -5,15 +5,15 @@ const { verifyAdmin } = require('../middleware/jwt.auth');
 const { taskAttachmentUpload } = require('../middleware/multer');
 const path = require('path');
 
-projectRoute.use('/download', verifyAdmin,express.static(path.join(__dirname, '../upload')));
+projectRoute.use('/download', verifyAdmin, express.static(path.join(__dirname, '../upload')));
 
 projectRoute.get('/getProjects', projectPath.getProjects);
 projectRoute.post('/addProject', verifyAdmin, projectPath.addProject);
 projectRoute.put('/update', verifyAdmin, projectPath.updateProject);
 projectRoute.put('/updateStatus', verifyAdmin, projectPath.updateStatus);
-projectRoute.post("/upload",verifyAdmin,taskAttachmentUpload.single('project'),projectPath.uploadProject_File)
-projectRoute.get("/getProjectName",verifyAdmin,projectPath.getallProject);
-projectRoute.get("/download/:filename",projectPath.download);
-projectRoute.get("/files",verifyAdmin,projectPath.allProjectFiles);
+projectRoute.post("/upload", verifyAdmin, taskAttachmentUpload.single('project'), projectPath.uploadProject_File)
+projectRoute.get("/getProjectName", verifyAdmin, projectPath.getallProject);
+// projectRoute.get("/download/:filename",projectPath.download);
+projectRoute.get("/files", verifyAdmin, projectPath.allProjectFiles);
 
 module.exports = projectRoute;
