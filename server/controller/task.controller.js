9500b73ab55d2d2accf5c1224657fc1201a882cc
FIRST_NAME LAST_NAME <89856336+LeenaDhiman@users.noreply.github.com>
2023-12-04 11:02:26 +0530

    create task - remove the history

--- server/controller/task.controller.js
+++ server/controller/task.controller.js
@@ -36,7 +36,6 @@ const createtask = async (req, res) => {
       if (task) {
         const roles = ['CTO', 'PM', 'Admin'];
         const role = await rolesModel.findOne({ role: roles.includes(req.user.role) ? req.user.role : "PM" }).select("_id role");
-        userHistory(req,res,"Create Task")
         const assignedUser = await assignUserModel.create({
           assigneeId: roles.includes(req.user.role) ? assigneeId : req.user._id,
           reporterId: role._id,
