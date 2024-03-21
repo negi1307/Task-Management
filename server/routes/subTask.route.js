fcfcc95c444ebd311fb53256aaba4fec8dba4caf
jtech72 <j.valecha@rowthtech.com>
2024-03-21 16:05:50 +0530

    change in API of get and add and update

--- server/routes/subTask.route.js
+++ server/routes/subTask.route.js
@@ -2,9 +2,10 @@ const express = require("express");
 const subTaskRoute = express.Router();
 const subTask = require('../controller/subTask.controller');
 const { verifyUser } = require("../middleware/jwt.auth");
+const { taskAttachmentUpload } = require('../middleware/multer');
 
-subTaskRoute.post('/addSubTask', verifyUser, subTask.addSubTask);
-subTaskRoute.put('/updateSubTask', verifyUser, subTask.updateSubTask);
+subTaskRoute.post('/addSubTask', verifyUser, taskAttachmentUpload.single('attachment'), subTask.addSubTask);
+subTaskRoute.put('/updateSubTask', verifyUser, taskAttachmentUpload.single('attachment'), subTask.updateSubTask);
 subTaskRoute.get('/getSubTask', verifyUser, subTask.getSubTask);
 subTaskRoute.delete('/deleteSubTask', verifyUser, subTask.deleteSubTask);
 subTaskRoute.put("/updateSubTaskStatus", verifyUser, subTask.updateSubTaskStatus);
