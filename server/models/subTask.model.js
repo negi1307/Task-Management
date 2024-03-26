4ec589c805183e716c7005b6d5a25e793202573f
jtech72 <j.valecha@rowthtech.com>
2024-03-26 15:18:25 +0530

    change status case

--- server/models/subTask.model.js
+++ server/models/subTask.model.js
@@ -28,7 +28,7 @@ const subTask = mongoose.model(
         },
         status: {
             type: Number,
-            default: 1 // 1=,todo, 2=inProgress, 3=hold, 4=done 
+            default: 1 // 1=,todo, 2=inProgress,3=testing,4=hold, 5=done 
         },
         inProgressDate: {
             type: Date
